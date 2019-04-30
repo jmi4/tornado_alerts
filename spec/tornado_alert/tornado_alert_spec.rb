8a4f007188b0f69f9facd93083324b06f8be4d19
Jeremy Miller <jeremy.j.miller@cerner.com>
2019-04-29 20:33:42 -0500

    updating spec again

---
+++ spec/tornado_alert/tornado_alert_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'DSTornadoAlert::TornadoAlert' do
+  let(:lat) { '38.8846' }
+  let(:long) { '-94.4971' }
+  let(:api_key) { 'your-api_key' }
+  let(:api_url) { 'https://api.darksky.net' }
+
+  context '#forecast' do
+    it 'should return a valid weather warning alert' do
+      VCR.use_cassette('forecast', record: :once) do
+        alerts = DSTornadoAlert::TornadoAlert.forecast
+        expect(alerts['title']).to eq('Tornado Warning')
+      end
+    end
+  end
+end
---
+++ spec/tornado_alert/version_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'DSTornadoAlert::VERSION' do
+  it 'should be the correct version' do
+    expect(DSTornadoAlert::VERSION).to eq('0.0.2')
+  end
+end
